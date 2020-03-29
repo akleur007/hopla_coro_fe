43cddc1158b3c9a6118148ab3297547787222d77
shufflerror_Win <wahn@shufflerror.com>
2020-03-29 15:40:00 +0200

    add ntry from email spliter

--- src/views/ViewBons.vue
+++ src/views/ViewBons.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="container">
+    <form class="ng-pristine" @submit="doSplitText()">
+          <div class="form-group row justify-content-between">
+              <div class="col-lg-12 col-sm-12 col-input">
+                  <textarea id="split-text" rows="6" cols="50" v-model="splitText" class="form-control" placeholder="paypal Textblock">
+                  </textarea>
+              </div>
+              <div class="col-lg-2 col-sm-12 col-input text-right">
+                  <button id="submit-it" type="submit" class="btn btn-primary">Separieren</button>
+              </div>
+          </div>
+      </form>
       <form class="ng-pristine" @submit="postEntry()">
           <div class="form-group row justify-content-between">
               <div class="col-lg-4 col-sm-12 col-input">
@@ -16,7 +27,6 @@
               </div>
           </div>
       </form>
-      <div id="message" v-html="messageDisplay"></div>
       <h2>Angelegte Bons:</h2>
       <ul id="bonList"  class="list-group-striped">
           <li class="bon-list-entry list-group-item" v-for="(entry) in entrys" :key="entry.id">
@@ -36,7 +46,7 @@ import {
 } from '../services/bonService';
 import BonListEntry from '../components/BonListEntry.vue';
 
-const creatRandomHash = () => {
+const createRandomHash = () => {
   const currentDate = (new Date()).valueOf().toString();
   const random = Math.random().toString();
   const hash = crypto.createHash('sha1').update(currentDate + random).digest('hex');
@@ -55,7 +65,7 @@ export default {
       name: '',
       email: '',
       credit: '',
-      messageDisplay: '',
+      splitText: '',
     };
   },
   created() {
@@ -69,28 +79,42 @@ export default {
         })
         .catch((e) => {
           this.errors.push(e);
-          this.messageDisplay = e;
         });
     },
     postEntry() {
+      const label = 'Bon';
       const params = {
         name: this.name,
         email: this.email,
         credit: parseFloat(this.credit, 10),
-        authKey: creatRandomHash(),
+        authKey: createRandomHash(),
       };
       createBon(params)
         .then(() => {
           this.flashMessage.show({
-            title: 'Bon geändert',
+            title: 'Bon angelegt',
             message: '',
             wrapperClass: 'msg alert-success',
           });
           this.listEntries();
         })
         .catch((e) => {
+          const serverError = e.response.data;
+          let msg = '';
+          switch (serverError.data.code) {
+            case 'ER_DUP_ENTRY':
+              msg = `${label} schon vergeben!`;
+              break;
+            default:
+              msg = `Fehler beim ${label} Anlegen`;
+              break;
+          }
           this.errors.push(e);
-          this.messageDisplay = e;
+          this.flashMessage.show({
+            title: 'Bon konnte nicht angelegt werden',
+            message: `${msg}`,
+            wrapperClass: 'msg alert-warning',
+          });
         });
     },
     deleteEntry(id) {
@@ -100,7 +124,6 @@ export default {
         })
         .catch((e) => {
           this.errors.push(e);
-          this.messageDisplay = e;
         });
     },
     sendEntryEmail(id) {
@@ -122,6 +145,18 @@ export default {
             wrapperClass: 'msg alert-warning',
           });
         });
+    },
+    doSplitText() {
+      const t = this.splitText;
+      const name = t.split(' hat Ihnen ')[0];
+      const credit = t.split(' hat Ihnen ')[1].split(' EUR')[0];
+      const email = t.split('quote')[1].replace(/\s/g, '');
+      this.name = name;
+      this.email = email;
+      this.credit = parseFloat(credit.replace(',', '.'), 10);
+      console.log(name);
+      console.log(credit);
+      console.log(email);
     },
   },
   components: {
